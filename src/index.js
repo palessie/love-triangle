69109a84934352bbc812361fa10475154210e6d2
Palesse <1869680@gmail.com>
2018-09-23 22:27:56 +0300

    1

--- src/index.js
+++ src/index.js
@@ -4,4 +4,24 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let count = 0;
+  let arr = [0];
+  preferences = arr.concat(preferences);
+
+  let i;
+  for (i = 1; i <= preferences.length; i++) {
+    let firstEl = preferences[i];
+    let secondEl = preferences[firstEl];
+    let thirdEl = preferences[secondEl];
+
+    if
+    ((thirdEl == i)
+      && (preferences[firstEl] != firstEl
+        && preferences[secondEl] != secondEl
+        && preferences[thirdEl] != thirdEl)
+    ) {
+      count++;
+    }
+  }
+  return count / 3;
 };
